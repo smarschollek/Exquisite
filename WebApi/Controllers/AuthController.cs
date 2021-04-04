767df59aaa355bb7f691d01c24c9c276367d8ec9
Stephan Marschollek <stephan.marschollek@gmail.com>
2021-04-04 10:57:42 +0200

    Add project files.

---
+++ WebApi/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("Login")]
+        public ActionResult Login()
+        {
+            return Ok();
+        }
+
+        [HttpPost("CheckToken")]
+        public ActionResult CheckToken()
+        {
+            return Ok();
+        }
+    }
+}
